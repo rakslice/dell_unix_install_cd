81ae5dc71a0042e1332a6097fefe314141f45dba
rakslice <rakslice@gmail.com>
2023-11-07 23:59:30 -0800

    handle base padding; make_iso run completion message

--- make_iso.bash
+++ make_iso.bash
@@ -15,12 +15,16 @@ mkdir "$cddir"
 ./make_boot.sh
 ./make_system.sh
 
+base_archive=original_images/base
+diff - <(sha256sum $base_archive) <<< "46539dbfbddb1cc1f4d03d82e4c390fef320935187ab266a41a7f0db1f22e154  $base_archive"
+
+./pad2k $base_archive "$cddir/base"
+
 boot_floppy_image=boot.cdramd_128.img
 cp $boot_floppy_image "$cddir"
 
 ./extract_system_fs.sh system_cd.cdramd_128.img
 cp system_fs "$cddir"
-cp base.pad.iso "$cddir/base"
 
 genisoimage -o $output_filename -sort sortfile -b $boot_floppy_image -lJR "$cddir"
 
@@ -39,6 +43,10 @@ dd if=$output_filename bs=2048 skip=$system_fs_offset count=$system_fs_blocks | 
 # base cpio archive
 #  offset=32 + 711 2k blocks
 base_offset=$(($system_fs_offset + $system_fs_blocks))
-base_size=$(stat -c '%s' base.pad.iso)
+base_size=$(stat -c '%s' "$cddir/base")
 base_blocks=$(($base_size / 2048))
-dd if=$output_filename bs=2048 skip=$base_offset count=$base_blocks | diff - base.pad.iso
+dd if=$output_filename bs=2048 skip=$base_offset count=$base_blocks | diff - "$cddir/base"
+
+set +x
+echo "CD image creation complete"
+echo "Output file: $output_filename"

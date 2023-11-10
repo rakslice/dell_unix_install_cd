c8d0de221f8b2868eeda8c9a0c9e3a91f3bb379f
rakslice <rakslice@gmail.com>
2023-11-10 00:30:31 -0800

    Note about the s5 filesystem offset

--- extract_system_fs.sh
+++ extract_system_fs.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 set -e -x -u
 
+# The s5 filesystem on these floppies is located one cylinder into the disk
+# (what it gets by using e.g. /dev/dsk/f0 as opposed to /dev/dsk/f0t),
+# to make room for the usual PC sector 0 and any other unmentionables
+# 1 cyl = 2 heads x 18 sectors/track = 36 sectors
 dd if="$1" bs=512 skip=36 > system_fs
 ls -l system_fs
